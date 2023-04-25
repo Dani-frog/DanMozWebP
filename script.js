02ebce8e45364ebcb12cb8e251e7b029caa1deb8
TothDaniJani <toth.daniel@ckik.hu>
2023-04-25 13:05:50 +0200

    kezdetek

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
+var jatekos=true;//true az elso, false akk a második;
+var jatekos1pont=0;
+var jatekos2pont=0;
+var tabla = document.getElementById("tabla");
+var jatekter = document.createElement("table");
+var Matrix=[];//kezdő helyek 28,29 ,,, 36,37
 
+function Jatekeleje(){
+//első korong koordináta
+tabla.appendChild(jatekter);
+var kep = document.createElement("img");
+kep.src= "kepek/"+1+".png";
+
+document.getElementById("1_1").appendChild(kep);
+}
 
 function tablaGen(){
-    var tabla = document.getElementById("tabla");
-    var jatekter = document.createElement("table");
-    var k = 0;
+    //var k = 0;
+    var s=1;
+    
     for(var i = 0;i<8;i++){
         var sor = document.createElement("tr");
+        var o=1;
         for(var j = 0;j<8;j++){
+            
             var oszlop = document.createElement("td");
             sor.appendChild(oszlop)
-            oszlop.id = k;
-            k++;
+            oszlop.id = s+"_"+o;
             //oszlop.style.border = "2px solid red";
+            Matrix.push(oszlop.id);
+            o++;
+
         }
         jatekter.appendChild(sor);
+        s++;
     }
-    tabla.appendChild(jatekter);
+    
 }
 tablaGen();
+Jatekeleje();
